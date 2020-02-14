35aee12b6d1538d87eff52c042e21c41f8edf80c
jsEveryDay <nedbasha@gmail.com>
2020-02-14 06:04:12 -0500

    Create youtube.js

---
+++ youtube.js
@@ -0,0 +1,23 @@
+//On the subscriptions menu on the left, click on the ICON of the channel to go directly to Uploads, as some channels are difficult to navigate.
+youtubeChannelUploads.js application/javascript
+(function() {
+	document.addEventListener('DOMContentLoaded', function() {
+		const el = document.querySelector("ytd-guide-section-renderer.ytd-guide-renderer.style-scope:nth-of-type(2)");
+		el.firstElementChild.firstElementChild.setAttribute(
+		    "style",
+		    "color:aqua; text-align: center;"
+		  );
+		el.lastElementChild.addEventListener(
+		"click",
+			function(event) {
+			  if (event.target.nodeName == "IMG") {
+			    location.href =
+			      event.target.parentNode.parentNode.parentNode.href +
+			      "/videos?view=0&sort=dd&shelf_id=1";
+			    event.stopImmediatePropagation();
+			  }
+			},
+			false
+		);
+	});
+})();
